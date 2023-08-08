67877742d7cf4c383c49b6f44d74379462f6311f
IsabelleCruz333 <isabellecruz333@gmail.com>
2023-08-08 11:47:22 -0300

    projeto-javascript

---
+++ script.js
@@ -0,0 +1,12 @@
+function inserir(){
+    let numero = document.getElementById('posicao').value -1
+    let novo = document.getElementById('novo').value 
+    document.getElementsByClassName('colocado')[numero].innerHTML=novo 
+
+}
+
+function mostrar(){
+    let numero= document.getElementById('numero').value -1
+    let mes= document.getElementsByTagName('li')[numero].innerHTML; 
+    document.getElementById('resultado').innerHTML = mes;
+}
